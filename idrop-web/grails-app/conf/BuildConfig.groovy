b241139b0a0d4cc8527a1ad72747f5dac0f960f0
Mike Conway <mikeconway@dhcp152-54-6-70.wireless.europa.renci.org>
2011-02-10 10:38:36 -0500

    updates to grails config, testing with updated jargon-core, added renci repos

--- idrop-web/grails-app/conf/BuildConfig.groovy
+++ idrop-web/grails-app/conf/BuildConfig.groovy
@@ -10,28 +10,30 @@ grails.project.dependency.resolution = {
     }
     log "warn" // LOG level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     repositories {
-        
-
         // uncomment the below to enable remote dependency resolution
         // from public Maven repositories
         mavenLocal()
         mavenCentral()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
+        mavenRepo "http://snapshots.repository.codehaus.org"
+		mavenRepo "http://repository.codehaus.org"
+        mavenRepo "http://download.java.net/maven/2/"
         mavenRepo "http://repository.jboss.com/maven2/"
+		mavenRepo "http://ci-dev.renci.org/nexus/content/repositories/release"
+		mavenRepo "http://ci-dev.renci.org/nexus/content/repositories/snapshots"
 		grailsPlugins()
 		grailsHome()
 		grailsCentral()
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-		test 'org.irods:jargon-test:2.4.1-SNAPSHOT'
-		compile 'org.irods:jargon-core:2.4.1-SNAPSHOT'
-		compile 'org.irods:jargon-security:2.4.1-SNAPSHOT'
+		test 'org.irods.jargon:jargon-test:2.4.1-SNAPSHOT'
+		compile 'org.irods.jargon:jargon-core:2.4.1-SNAPSHOT'
+		compile 'org.irods.jargon:jargon-security:2.4.1-SNAPSHOT'
 		compile 'org.springframework.security:spring-security-core:3.0.5.RELEASE'
 		compile 'org.springframework.security:spring-security-web:3.0.5.RELEASE'
 		compile 'org.springframework.security:spring-security-config:3.0.5.RELEASE'
+		compile 'org.springframework:spring-web:3.0.5.RELEASE'
+		compile 'javax.servlet:servlet-api:2.5'
 		
 		compile( group: 'log4j', name: 'log4j', version: '1.2.16', export: false )
 		//compile 'log4j:log4j:1.2.16'
