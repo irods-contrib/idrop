b98d633b02e3c53baf26d1b2fbe0da45649ed9c1
Jason Reilly <jdr0887@renci.org>
2011-02-21 10:19:55 -0500

    using iDropCore & adding preferences to login parameters

--- idrop-swing/src/main/java/org/irods/jargon/idrop/desktop/systraygui/LoginDialog.java
+++ idrop-swing/src/main/java/org/irods/jargon/idrop/desktop/systraygui/LoginDialog.java
@@ -1,69 +1,88 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/*
- * LoginDialog.java
- *
- * Created on Jul 7, 2010, 4:41:29 PM
- */
 package org.irods.jargon.idrop.desktop.systraygui;
 
 import java.awt.Color;
 import java.awt.event.ActionEvent;
-import java.beans.PropertyChangeListener;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.prefs.Preferences;
+
 import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.JComponent;
+import javax.swing.JDialog;
 import javax.swing.KeyStroke;
+
+import org.apache.commons.lang.StringUtils;
 import org.irods.jargon.core.connection.IRODSAccount;
 import org.irods.jargon.core.exception.JargonException;
 import org.irods.jargon.core.pub.IRODSFileSystem;
 import org.irods.jargon.core.pub.UserAO;
 import org.irods.jargon.core.pub.domain.User;
-import org.irods.jargon.idrop.desktop.systraygui.utils.IdropConfig;
 import org.irods.jargon.idrop.desktop.systraygui.utils.IdropPropertiesHelper;
 import org.irods.jargon.idrop.exceptions.IdropException;
 import org.slf4j.LoggerFactory;
 
 /**
- *
+ * 
  * @author mikeconway
  */
-public class LoginDialog extends javax.swing.JDialog {
+public class LoginDialog extends JDialog {
 
-    private iDrop iDropParent = null;
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    private static final String PREF_LOGIN_HOST = "login.host";
+
+    private static final String PREF_LOGIN_ZONE = "login.zone";
+
+    private static final String PREF_LOGIN_RESOURCE = "login.resource";
+
+    private static final String PREF_LOGIN_USERNAME = "login.username";
+
+    private iDrop iDrop = null;
+
     public static org.slf4j.Logger log = LoggerFactory.getLogger(LoginDialog.class);
-    private IdropConfig idropConfig = null;
 
-    /** Creates new form LoginDialog */
-    public LoginDialog(iDrop iDropParent) {
-        super(iDropParent, true);
-        this.iDropParent = iDropParent;
+    private Preferences preferences = Preferences.userRoot();
+
+    public LoginDialog(iDrop iDrop) {
+        super(iDrop, true);
+        this.iDrop = iDrop;
         initComponents();
-        try {
-            // get the properties that determine the layout of the login dialog
-            idropConfig = IdropConfig.instance();
 
-            if (idropConfig.isLoginPreset()) {
-                log.debug("login will use presets");
-                lblHost.setVisible(false);
-                txtHost.setVisible(false);
-                lblPort.setVisible(false);
-                txtPort.setVisible(false);
-                lblZone.setVisible(false);
-                txtZone.setVisible(false);
-                lblResource.setVisible(false);
-                txtResource.setVisible(false);
-            }
+        // predispose based on preferences
+        String host = this.preferences.get(PREF_LOGIN_HOST, null);
+        if (StringUtils.isNotEmpty(host)) {
+            txtHost.setText(host);
+        }
 
-        } catch (IdropException ex) {
-            Logger.getLogger(LoginDialog.class.getName()).log(Level.SEVERE, null, ex);
-            iDropParent.showIdropException(ex);
-            System.exit(1);
+        String zone = this.preferences.get(PREF_LOGIN_ZONE, null);
+        if (StringUtils.isNotEmpty(zone)) {
+            txtZone.setText(zone);
+        }
+
+        String resource = this.preferences.get(PREF_LOGIN_RESOURCE, null);
+        if (StringUtils.isNotEmpty(resource)) {
+            txtResource.setText(resource);
+        }
+
+        String username = this.preferences.get(PREF_LOGIN_USERNAME, null);
+        if (StringUtils.isNotEmpty(username)) {
+            txtUserName.setText(username);
+        }
+
+        if (iDrop.getiDropCore().getIdropConfig().isLoginPreset()) {
+            log.debug("login will use presets");
+            lblHost.setVisible(false);
+            txtHost.setVisible(false);
+            lblPort.setVisible(false);
+            txtPort.setVisible(false);
+            lblZone.setVisible(false);
+            txtZone.setVisible(false);
+            lblResource.setVisible(false);
+            txtResource.setVisible(false);
         }
 
         registerKeystrokeListener();
@@ -72,12 +91,13 @@ public class LoginDialog extends javax.swing.JDialog {
 
     /**
      * Action to take when login is initiated
+     * 
      * @return
      * @throws NumberFormatException
      */
     private boolean processLogin() throws NumberFormatException {
-        //validate various inputs based on whether a full login, or a uid only login is indicated
-        if (!idropConfig.isLoginPreset()) {
+        // validate various inputs based on whether a full login, or a uid only login is indicated
+        if (!iDrop.getiDropCore().getIdropConfig().isLoginPreset()) {
             txtHost.setBackground(Color.white);
             txtPort.setBackground(Color.white);
             txtZone.setBackground(Color.white);
@@ -113,56 +133,70 @@ public class LoginDialog extends javax.swing.JDialog {
         final IRODSAccount irodsAccount;
         try {
             // validated, now try to log in
-            if (idropConfig.isLoginPreset()) {
+            if (iDrop.getiDropCore().getIdropConfig().isLoginPreset()) {
                 log.debug("creating account with presets");
-                String presetHost = idropConfig.getIdropProperties().getProperty(IdropPropertiesHelper.LOGIN_PRESET_HOST);
+                String presetHost = iDrop.getiDropCore().getIdropConfig().getIdropProperties()
+                        .getProperty(IdropPropertiesHelper.LOGIN_PRESET_HOST);
                 log.info("presetHost:{}", presetHost);
-                int presetPort = Integer.parseInt(idropConfig.getIdropProperties().getProperty(IdropPropertiesHelper.LOGIN_PRESET_PORT));
+                int presetPort = Integer.parseInt(iDrop.getiDropCore().getIdropConfig().getIdropProperties()
+                        .getProperty(IdropPropertiesHelper.LOGIN_PRESET_PORT));
                 log.info("presetPort:{}", presetPort);
-                String presetZone = idropConfig.getIdropProperties().getProperty(IdropPropertiesHelper.LOGIN_PRESET_ZONE);
+                String presetZone = iDrop.getiDropCore().getIdropConfig().getIdropProperties()
+                        .getProperty(IdropPropertiesHelper.LOGIN_PRESET_ZONE);
                 log.info("presetZone:{}", presetZone);
-                String presetResource = idropConfig.getIdropProperties().getProperty(IdropPropertiesHelper.LOGIN_PRESET_RESOURCE);
+                String presetResource = iDrop.getiDropCore().getIdropConfig().getIdropProperties()
+                        .getProperty(IdropPropertiesHelper.LOGIN_PRESET_RESOURCE);
                 log.info("presetResource:{}", presetResource);
                 sb.append('/');
                 sb.append(presetZone);
                 sb.append("/home/");
                 sb.append(txtUserName.getText());
-                irodsAccount = IRODSAccount.instance(presetHost, presetPort, txtUserName.getText(), new String(password.getPassword()), sb.toString(), presetZone, presetResource);
+                irodsAccount = IRODSAccount.instance(presetHost, presetPort, txtUserName.getText(),
+                        new String(password.getPassword()), sb.toString(), presetZone, presetResource);
             } else {
                 sb.append('/');
                 sb.append(txtZone.getText());
                 sb.append("/home/");
                 sb.append(txtUserName.getText());
-                irodsAccount = IRODSAccount.instance(txtHost.getText(), Integer.parseInt(txtPort.getText()), txtUserName.getText(), new String(password.getPassword()), sb.toString(), txtZone.getText(), txtResource.getText());
+                irodsAccount = IRODSAccount.instance(txtHost.getText(), Integer.parseInt(txtPort.getText()),
+                        txtUserName.getText(), new String(password.getPassword()), sb.toString(), txtZone.getText(),
+                        txtResource.getText());
             }
         } catch (JargonException ex) {
             Logger.getLogger(LoginDialog.class.getName()).log(Level.SEVERE, null, ex);
-            iDropParent.showIdropException(ex);
+            iDrop.showIdropException(ex);
             return true;
         }
+
+        // I figure at this point, it's safe to set the preferences...note that we are not caching password
+        preferences.put(PREF_LOGIN_HOST, txtHost.getText());
+        preferences.put(PREF_LOGIN_ZONE, txtZone.getText());
+        preferences.put(PREF_LOGIN_RESOURCE, txtResource.getText());
+        preferences.put(PREF_LOGIN_USERNAME, txtUserName.getText());
+
         IRODSFileSystem irodsFileSystem = null;
         try {
             irodsFileSystem = IRODSFileSystem.instance();
             final UserAO userAO = irodsFileSystem.getIRODSAccessObjectFactory().getUserAO(irodsAccount);
             final User loggedInUser = userAO.findByName(txtUserName.getText());
-            iDropParent.setIrodsAccount(irodsAccount);
+            iDrop.setIrodsAccount(irodsAccount);
             this.dispose();
         } catch (JargonException ex) {
             if (ex.getMessage().indexOf("Connection refused") > -1) {
                 Logger.getLogger(LoginDialog.class.getName()).log(Level.SEVERE, null, ex);
-                iDropParent.showMessageFromOperation("Cannot connect to the server, is it down?");
+                iDrop.showMessageFromOperation("Cannot connect to the server, is it down?");
                 return true;
             } else if (ex.getMessage().indexOf("Connection reset") > -1) {
                 Logger.getLogger(LoginDialog.class.getName()).log(Level.SEVERE, null, ex);
-                iDropParent.showMessageFromOperation("Cannot connect to the server, is it down?");
+                iDrop.showMessageFromOperation("Cannot connect to the server, is it down?");
                 return true;
             } else if (ex.getMessage().indexOf("io exception opening socket") > -1) {
                 Logger.getLogger(LoginDialog.class.getName()).log(Level.SEVERE, null, ex);
-                iDropParent.showMessageFromOperation("Cannot connect to the server, is it down?");
+                iDrop.showMessageFromOperation("Cannot connect to the server, is it down?");
                 return true;
             } else {
                 Logger.getLogger(LoginDialog.class.getName()).log(Level.SEVERE, null, ex);
-                iDropParent.showMessageFromOperation("login error - unable to log in, or invalid user id");
+                iDrop.showMessageFromOperation("login error - unable to log in, or invalid user id");
                 return true;
             }
         } finally {
@@ -189,17 +223,13 @@ public class LoginDialog extends javax.swing.JDialog {
                 processLogin();
             }
         };
-        btnOK.registerKeyboardAction(enterAction, enter,
-                             JComponent.WHEN_IN_FOCUSED_WINDOW);
+        btnOK.registerKeyboardAction(enterAction, enter, JComponent.WHEN_IN_FOCUSED_WINDOW);
 
-        }
+    }
 
-
-
-    /** This method is called from within the constructor to
-     * initialize the form.
-     * WARNING: Do NOT modify this code. The content of this method is
-     * always regenerated by the Form Editor.
+    /**
+     * This method is called from within the constructor to initialize the form. WARNING: Do NOT modify this code. The
+     * content of this method is always regenerated by the Form Editor.
      */
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -297,7 +327,7 @@ public class LoginDialog extends javax.swing.JDialog {
         gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
         pnlLoginInfo.add(password, gridBagConstraints);
 
-        getContentPane().add(pnlLoginInfo, java.awt.BorderLayout.CENTER);
+        this.getContentPane().add(pnlLoginInfo, java.awt.BorderLayout.CENTER);
 
         pnlToolbar.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.RIGHT, 2, 5));
 
@@ -319,38 +349,55 @@ public class LoginDialog extends javax.swing.JDialog {
         });
         pnlToolbar.add(btnCancel);
 
-        getContentPane().add(pnlToolbar, java.awt.BorderLayout.SOUTH);
+        this.getContentPane().add(pnlToolbar, java.awt.BorderLayout.SOUTH);
 
         lblLogin.setText("Please log in to your iDrop data grid");
-        getContentPane().add(lblLogin, java.awt.BorderLayout.PAGE_START);
+        this.getContentPane().add(lblLogin, java.awt.BorderLayout.PAGE_START);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnOKActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnOKActionPerformed
-      processLogin();
-    }//GEN-LAST:event_btnOKActionPerformed
+    private void btnOKActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_btnOKActionPerformed
+        processLogin();
+    }// GEN-LAST:event_btnOKActionPerformed
 
-    private void btnCancelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCancelActionPerformed
+    private void btnCancelActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_btnCancelActionPerformed
         System.exit(0);
-    }//GEN-LAST:event_btnCancelActionPerformed
-    // Variables declaration - do not modify//GEN-BEGIN:variables
+    }// GEN-LAST:event_btnCancelActionPerformed
+     // Variables declaration - do not modify//GEN-BEGIN:variables
+
     private javax.swing.JButton btnCancel;
+
     private javax.swing.JButton btnOK;
+
     private javax.swing.JLabel lblHost;
+
     private javax.swing.JLabel lblLogin;
+
     private javax.swing.JLabel lblPassword;
+
     private javax.swing.JLabel lblPort;
+
     private javax.swing.JLabel lblResource;
+
     private javax.swing.JLabel lblUserName;
+
     private javax.swing.JLabel lblZone;
+
     private javax.swing.JPasswordField password;
+
     private javax.swing.JPanel pnlLoginInfo;
+
     private javax.swing.JPanel pnlToolbar;
+
     private javax.swing.JTextField txtHost;
+
     private javax.swing.JTextField txtPort;
+
     private javax.swing.JTextField txtResource;
+
     private javax.swing.JTextField txtUserName;
+
     private javax.swing.JTextField txtZone;
     // End of variables declaration//GEN-END:variables
 }
