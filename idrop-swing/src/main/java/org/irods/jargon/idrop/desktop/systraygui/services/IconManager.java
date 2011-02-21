5cf48909d0b4b8964436699f75546a803dfc492a
Jason Reilly <jdr0887@renci.org>
2011-02-21 10:24:12 -0500

    removing instance method since it does the same thing as new...not a singleton, no need for instance

--- idrop-swing/src/main/java/org/irods/jargon/idrop/desktop/systraygui/services/IconManager.java
+++ idrop-swing/src/main/java/org/irods/jargon/idrop/desktop/systraygui/services/IconManager.java
@@ -1,4 +1,3 @@
-
 package org.irods.jargon.idrop.desktop.systraygui.services;
 
 import org.irods.jargon.idrop.desktop.systraygui.iDrop;
@@ -6,19 +5,18 @@ import org.irods.jargon.transferengine.TransferManager;
 
 /**
  * Manager of icons for the system gui based on the status.
+ * 
  * @author Mike Conway - DICE (www.irods.org)
  */
 public class IconManager {
 
     private TransferManager.ErrorStatus errorStatus = null;
+
     private TransferManager.RunningStatus runningStatus = null;
+
     private final iDrop idropGui;
 
-    public static IconManager instance(final iDrop idropClient) {
-        return new IconManager(idropClient);
-    }
-
-    private IconManager(final iDrop idropClient) {
+    public IconManager(final iDrop idropClient) {
         this.idropGui = idropClient;
     }
 
@@ -35,17 +33,17 @@ public class IconManager {
     private void updateIcon() {
         String iconFile = "";
         if (runningStatus == TransferManager.RunningStatus.PAUSED) {
-            iconFile="images/media-playback-pause-3.png";
-        } else  if (errorStatus == TransferManager.ErrorStatus.ERROR) {
+            iconFile = "images/media-playback-pause-3.png";
+        } else if (errorStatus == TransferManager.ErrorStatus.ERROR) {
             iconFile = "images/dialog-error-3.png";
         } else if (errorStatus == TransferManager.ErrorStatus.WARNING) {
             iconFile = "images/dialog-warning.png";
-        } else if(runningStatus == TransferManager.RunningStatus.IDLE) {
+        } else if (runningStatus == TransferManager.RunningStatus.IDLE) {
             iconFile = "images/dialog-ok-2.png";
-        } else if(runningStatus == TransferManager.RunningStatus.PROCESSING) {
+        } else if (runningStatus == TransferManager.RunningStatus.PROCESSING) {
             iconFile = "images/system-run-5.png";
         } else {
-           iconFile = "images/dialog-ok-2.png";
+            iconFile = "images/dialog-ok-2.png";
         }
         idropGui.updateIcon(iconFile);
     }
