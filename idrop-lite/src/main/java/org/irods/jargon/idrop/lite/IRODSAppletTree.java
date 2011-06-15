9374203ab417092002626dd640121c4ab7c67670
LISA STILLWELL <lisa@dhcp152-54-6-218.wireless.europa.renci.org>
2011-06-15 14:47:00 -0400

    Added new idrop-commons package for classes that are common to idrop-swing and idrop-lite

---
+++ idrop-lite/src/main/java/org/irods/jargon/idrop/lite/IRODSAppletTree.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.irods.jargon.idrop.lite;
+
+import org.irods.jargon.idrop.commons.IRODSTree;
+import org.irods.jargon.idrop.commons.IRODSTreeTransferHandler;
+
+
+/**
+ *
+ * @author lisa
+ */
+public  class IRODSAppletTree extends IRODSTree  {
+
+
+    @Override
+    protected void setTransferHandler(IRODSTreeTransferHandler irodsTree_TH) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
