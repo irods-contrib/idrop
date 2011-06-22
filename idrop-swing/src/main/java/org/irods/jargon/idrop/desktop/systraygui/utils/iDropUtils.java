b58c61f2fa8ea58a59aa52bf2e0723a0784b7bec
Jason Reilly <jdr0887@renci.org>
2011-06-22 12:53:54 -0400

    organizing imports and formatting

--- idrop-swing/src/main/java/org/irods/jargon/idrop/desktop/systraygui/utils/iDropUtils.java
+++ idrop-swing/src/main/java/org/irods/jargon/idrop/desktop/systraygui/utils/iDropUtils.java
@@ -1,26 +1,26 @@
-
 package org.irods.jargon.idrop.desktop.systraygui.utils;
 
 /**
  * Misc iDrop utilities
+ * 
  * @author Mike Conway - DICE (www.irods.org)
  */
 public class iDropUtils {
-    
-    
+
     /**
      * Given a file name, return the name up to 100 chars, otherwise, redact and abbreviate
+     * 
      * @param fileName
-     * @return 
+     * @return
      */
     public static final String abbreviateFileName(final String fileName) {
-        
+
         if (fileName == null) {
             throw new IllegalArgumentException("null fileName");
         }
-        
+
         StringBuilder sb = new StringBuilder();
-        if(fileName.length() < 100) {
+        if (fileName.length() < 100) {
             sb.append(fileName);
         } else {
             // gt 100 bytes, redact
@@ -28,9 +28,9 @@ public class iDropUtils {
             sb.append(" ... ");
             sb.append(fileName.substring(fileName.length() - 50));
         }
-        
+
         return sb.toString();
-        
+
     }
-    
+
 }
