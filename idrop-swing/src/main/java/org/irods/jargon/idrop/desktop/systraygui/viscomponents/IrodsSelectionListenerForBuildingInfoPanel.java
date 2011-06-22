a26fdfbcc8613350e7671fcc47ee23fb26e980ce
Jason Reilly <jdr0887@renci.org>
2011-06-22 12:53:54 -0400

    organizing imports and formatting

--- idrop-swing/src/main/java/org/irods/jargon/idrop/desktop/systraygui/viscomponents/IrodsSelectionListenerForBuildingInfoPanel.java
+++ idrop-swing/src/main/java/org/irods/jargon/idrop/desktop/systraygui/viscomponents/IrodsSelectionListenerForBuildingInfoPanel.java
@@ -5,12 +5,12 @@ package org.irods.jargon.idrop.desktop.systraygui.viscomponents;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.event.TreeExpansionEvent;
-import javax.swing.event.TreeExpansionListener;
 import javax.swing.event.TreeSelectionEvent;
-import javax.swing.event.TreeSelectionListener;
+
 import org.irods.jargon.core.pub.CollectionAO;
 import org.irods.jargon.core.pub.DataObjectAO;
 import org.irods.jargon.core.pub.domain.Collection;
@@ -22,17 +22,19 @@ import org.irods.jargon.idrop.exceptions.IdropRuntimeException;
 import org.slf4j.LoggerFactory;
 
 /**
- * Class to encapsulate handling of info panel.  This object will listen to tree selection events in the iDROP
- * iRODS tree, and initialize the info panel.
- *
- *
- *
+ * Class to encapsulate handling of info panel. This object will listen to tree selection events in the iDROP iRODS
+ * tree, and initialize the info panel.
+ * 
+ * 
+ * 
  * @author Mike Conway - DICE (www.irods.org)
  */
 public class IrodsSelectionListenerForBuildingInfoPanel implements ListSelectionListener {
 
     public static org.slf4j.Logger log = LoggerFactory.getLogger(IrodsSelectionListenerForBuildingInfoPanel.class);
+
     private final iDrop idrop;
+
     private IRODSOutlineModel irodsFileSystemModel = null;
 
     public IrodsSelectionListenerForBuildingInfoPanel(final iDrop idrop) throws IdropException {
@@ -45,36 +47,30 @@ public class IrodsSelectionListenerForBuildingInfoPanel implements ListSelection
     }
 
     public void valueChanged(TreeSelectionEvent e) {
-//FIXME: reimplement
+        // FIXME: reimplement
 
         /*
-        if (!(idrop.getIrodsTree().getLastSelectedPathComponent() instanceof IRODSNode)) {
-        log.info("last selected is not a Node");
-        return;
-        }
-        
-        
-        final IRODSNode node = (IRODSNode) idrop.getIrodsTree().getLastSelectedPathComponent();
-        try {
-        identifyNodeTypeAndInitializeInfoPanel(node);
-        } catch (IdropException ex) {
-        Logger.getLogger(IrodsTreeListenerForBuildingInfoPanel.class.getName()).log(Level.SEVERE, null, ex);
-        throw new IdropRuntimeException("exception processing valueChanged() event for IRODSNode selection");
-        }
-        
-         *  * 
+         * if (!(idrop.getIrodsTree().getLastSelectedPathComponent() instanceof IRODSNode)) {
+         * log.info("last selected is not a Node"); return; }
+         * 
+         * 
+         * final IRODSNode node = (IRODSNode) idrop.getIrodsTree().getLastSelectedPathComponent(); try {
+         * identifyNodeTypeAndInitializeInfoPanel(node); } catch (IdropException ex) {
+         * Logger.getLogger(IrodsTreeListenerForBuildingInfoPanel.class.getName()).log(Level.SEVERE, null, ex); throw
+         * new IdropRuntimeException("exception processing valueChanged() event for IRODSNode selection"); }
+         * 
+         * *
          */
     }
 
     public void treeExpanded(TreeExpansionEvent event) {
-        /*TreePath expandedTreePath = event.getPath();
-        IRODSNode expandedNode = (IRODSNode) expandedTreePath.getLastPathComponent();
-        try {
-        identifyNodeTypeAndInitializeInfoPanel(expandedNode);
-        } catch (IdropException ex) {
-        Logger.getLogger(IrodsTreeListenerForBuildingInfoPanel.class.getName()).log(Level.SEVERE, null, ex);
-        throw new IdropRuntimeException("exception processing treeExpanded() event for IRODSNode selection");
-        }*/
+        /*
+         * TreePath expandedTreePath = event.getPath(); IRODSNode expandedNode = (IRODSNode)
+         * expandedTreePath.getLastPathComponent(); try { identifyNodeTypeAndInitializeInfoPanel(expandedNode); } catch
+         * (IdropException ex) {
+         * Logger.getLogger(IrodsTreeListenerForBuildingInfoPanel.class.getName()).log(Level.SEVERE, null, ex); throw
+         * new IdropRuntimeException("exception processing treeExpanded() event for IRODSNode selection"); }
+         */
     }
 
     public void treeCollapsed(TreeExpansionEvent event) {
@@ -83,6 +79,7 @@ public class IrodsSelectionListenerForBuildingInfoPanel implements ListSelection
 
     /**
      * Look at the kind of irods node and handle appropriately
+     * 
      * @param irodsNode
      * @throws IdropException
      */
@@ -103,14 +100,20 @@ public class IrodsSelectionListenerForBuildingInfoPanel implements ListSelection
 
     /**
      * When a selected node in the iRODS tree is a data object, put the data object info in the info panel
+     * 
      * @param irodsNode
      */
-    private void buildDataObjectFromSelectedIRODSNodeAndGiveToInfoPanel(final IRODSNode irodsNode) throws IdropException {
+    private void buildDataObjectFromSelectedIRODSNodeAndGiveToInfoPanel(final IRODSNode irodsNode)
+            throws IdropException {
         try {
-            CollectionAndDataObjectListingEntry collectionAndDataObjectListingEntry = (CollectionAndDataObjectListingEntry) irodsNode.getUserObject();
+            CollectionAndDataObjectListingEntry collectionAndDataObjectListingEntry = (CollectionAndDataObjectListingEntry) irodsNode
+                    .getUserObject();
             log.info("will be getting a data object based on entry in IRODSNode:{}", irodsNode);
-            DataObjectAO dataObjectAO = idrop.getiDropCore().getIRODSAccessObjectFactory().getDataObjectAO(idrop.getIrodsAccount());
-            DataObject dataObject = dataObjectAO.findByCollectionNameAndDataName(collectionAndDataObjectListingEntry.getParentPath(), collectionAndDataObjectListingEntry.getPathOrName());
+            DataObjectAO dataObjectAO = idrop.getiDropCore().getIRODSAccessObjectFactory()
+                    .getDataObjectAO(idrop.getIrodsAccount());
+            DataObject dataObject = dataObjectAO.findByCollectionNameAndDataName(
+                    collectionAndDataObjectListingEntry.getParentPath(),
+                    collectionAndDataObjectListingEntry.getPathOrName());
             idrop.initializeInfoPanel(dataObject);
         } catch (Exception e) {
             log.error("error building data object for: {}", irodsNode);
@@ -120,14 +123,19 @@ public class IrodsSelectionListenerForBuildingInfoPanel implements ListSelection
 
     /**
      * When a selected node in the iRODS tree is a collection, put the collection info into the info panel
+     * 
      * @param irodsNode
      */
-    private void buildCollectionFromSelectedIRODSNodeAndGiveToInfoPanel(final IRODSNode irodsNode) throws IdropException {
+    private void buildCollectionFromSelectedIRODSNodeAndGiveToInfoPanel(final IRODSNode irodsNode)
+            throws IdropException {
         try {
-            CollectionAndDataObjectListingEntry collectionAndDataObjectListingEntry = (CollectionAndDataObjectListingEntry) irodsNode.getUserObject();
+            CollectionAndDataObjectListingEntry collectionAndDataObjectListingEntry = (CollectionAndDataObjectListingEntry) irodsNode
+                    .getUserObject();
             log.info("will be getting a collection based on entry in IRODSNode:{}", irodsNode);
-            CollectionAO collectionAO = idrop.getiDropCore().getIRODSAccessObjectFactory().getCollectionAO(idrop.getIrodsAccount());
-            Collection collection = collectionAO.findByAbsolutePath(collectionAndDataObjectListingEntry.getPathOrName());
+            CollectionAO collectionAO = idrop.getiDropCore().getIRODSAccessObjectFactory()
+                    .getCollectionAO(idrop.getIrodsAccount());
+            Collection collection = collectionAO
+                    .findByAbsolutePath(collectionAndDataObjectListingEntry.getPathOrName());
             idrop.initializeInfoPanel(collection);
         } catch (Exception e) {
             log.error("error building collection objectt for: {}", irodsNode);
