8f94f6154458724c1583bd572804756a407f2431
Mike Conway <mikeconway@Mike-Conways-MacBook-Pro.local>
2011-10-01 12:04:24 -0400

    [#479] audit trail support in idrop web

--- idrop-web/grails-app/controllers/org/irods/mydrop/controller/AuditController.groovy
+++ idrop-web/grails-app/controllers/org/irods/mydrop/controller/AuditController.groovy
@@ -1,6 +1,33 @@
 package org.irods.mydrop.controller
 
+import org.irods.jargon.core.connection.IRODSAccount
+import org.irods.jargon.core.exception.JargonRuntimeException
+import org.irods.jargon.core.pub.IRODSAccessObjectFactory
+import org.springframework.security.context.SecurityContextHolder
+
 class AuditController {
+	
+	IRODSAccessObjectFactory irodsAccessObjectFactory
+	IRODSAccount irodsAccount
+
+	/**
+	 * Interceptor grabs IRODSAccount from the SecurityContextHolder
+	 */
+	def beforeInterceptor = {
+		def irodsAuthentication = SecurityContextHolder.getContext().authentication
+
+		if (irodsAuthentication == null) {
+			throw new JargonRuntimeException("no irodsAuthentication in security context!")
+		}
+
+		irodsAccount = irodsAuthentication.irodsAccount
+		log.debug("retrieved account for request: ${irodsAccount}")
+	}
+
+	def afterInterceptor = {
+		log.debug("closing the session")
+		irodsAccessObjectFactory.closeSession()
+	}
 	
     def auditList = { 
 		
