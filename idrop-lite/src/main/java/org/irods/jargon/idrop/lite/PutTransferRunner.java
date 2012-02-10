870e1b94d64becacb7ae457dcfbe97e9ad4ccf39
LISA STILLWELL <lisa@dhcp152-54-6-218.wireless.europa.renci.org>
2012-02-10 13:37:09 -0500

    [#626] just needed to set intrafilestatuscallbacks to true in transfercontrolblocks transfer options

--- idrop-lite/src/main/java/org/irods/jargon/idrop/lite/PutTransferRunner.java
+++ idrop-lite/src/main/java/org/irods/jargon/idrop/lite/PutTransferRunner.java
@@ -1,17 +1,7 @@
 package org.irods.jargon.idrop.lite;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.List;
-
 import org.irods.jargon.core.exception.JargonException;
-import org.irods.jargon.core.packinstr.TransferOptions;
-import org.irods.jargon.core.pub.DataTransferOperations;
-import org.irods.jargon.core.transfer.DefaultTransferControlBlock;
 import org.irods.jargon.core.transfer.TransferControlBlock;
 import org.slf4j.LoggerFactory;
 
@@ -75,11 +65,10 @@ public class PutTransferRunner implements Runnable {
 
         // need to create new Transfer Control Block for each transfer since it needs to be reset
         // on how many files there are to transfer and how many have been transferred so far
-        TransferControlBlock tcb = null;;
         try {
-            tcb = DefaultTransferControlBlock.instance();
-            idropGui.getiDropCore().setTransferControlBlock(tcb);
-            this.transferControlBlock = tcb;
+        	this.transferControlBlock =  idropGui.getiDropCore().getIrodsFileSystem().getIRODSAccessObjectFactory().buildDefaultTransferControlBlockBasedOnJargonProperties();
+            transferControlBlock.getTransferOptions().setIntraFileStatusCallbacks(true); 
+            idropGui.getiDropCore().setTransferControlBlock(transferControlBlock);
         } catch (JargonException ex) {
             java.util.logging.Logger.getLogger(LocalFileTree.class.getName()).log(
                     java.util.logging.Level.SEVERE, null, ex);
@@ -105,15 +94,14 @@ public class PutTransferRunner implements Runnable {
         String localSourceAbsolutePath = uploadData.getFileName();
         String sourceResource = idropGui.getIrodsAccount().getDefaultStorageResource();
 
-// need to create new Transfer Control Block for each transfer since it needs to be reset
-// on how many files there are to transfer and how many have been transferred so far
-       
+		// need to create new Transfer Control Block for each transfer since it needs to be reset
+		// on how many files there are to transfer and how many have been transferred so far
         log.info("initiating put transfer");
         try {
            this.transferControlBlock =  idropGui.getiDropCore().getIrodsFileSystem().getIRODSAccessObjectFactory().buildDefaultTransferControlBlockBasedOnJargonProperties();
            transferControlBlock.getTransferOptions().setIntraFileStatusCallbacks(true); 
            idropGui.getiDropCore().setTransferControlBlock(transferControlBlock);
-            idropGui.getiDropCore().getTransferManager().putOperationURL(localSourceAbsolutePath,
+           idropGui.getiDropCore().getTransferManager().putOperationURL(localSourceAbsolutePath,
                     targetIrodsFileAbsolutePath, sourceResource, idropGui, transferControlBlock);
         } catch (JargonException ex) {
             java.util.logging.Logger.getLogger(LocalFileTree.class.getName()).log(
