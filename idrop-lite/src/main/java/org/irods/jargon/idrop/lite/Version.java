7eef235f421d7f170859aee6e720b01ad59e7398
mconway <michael.c.conway@gmail.com>
2012-04-13 14:35:20 -0400

    [#725] version 1.0.0 release activities

--- idrop-lite/src/main/java/org/irods/jargon/idrop/lite/Version.java
+++ idrop-lite/src/main/java/org/irods/jargon/idrop/lite/Version.java
@@ -1,4 +1,4 @@
 package org.irods.jargon.idrop.lite;
 public final class Version {
- public static String VERSION="20120413-0812";
+ public static String VERSION="20120413-0834";
 }
--- idrop-web/grails-app/conf/Config.groovy
+++ idrop-web/grails-app/conf/Config.groovy
@@ -23,12 +23,11 @@ environments {
  * 2) If you want to set things up so that the idrop web browser automatically points to a certain grid (i.e. only user ID and password show, then do something like below.
  * Take the following outside of the comment block and configure here
  * 
- idrop.config.preset.host="diamond.ils.unc.edu
+ idrop.config.preset.host="diamond.ils.unc.edu"
  idrop.config.preset.port="2247"
  idrop.config.preset.zone="lifelibZone"
  idrop.config.preset.resource="lifelibResc1"
  */
-
 
 /*
  * 3) iDROP web includes the idrop-lite Java applet, which is launched from the iDROP web interface.  The interface needs to know where to find this jar file.
@@ -44,10 +43,9 @@ idrop.config.idrop.lite.codebase="https://lifetime-library.ils.unc.edu/llclient"
 idrop.config.idrop.lite.use.applet.dir=false
  */
 
-idrop.config.idrop.lite.applet.jar="idrop-lite-1.0.0-jar-with-dependencies.jar"
+idrop.config.idrop.lite.applet.jar="idrop-lite-1.0.0-SNAPSHOT-jar-with-dependencies.jar"
 idrop.config.idrop.lite.codebase="http://iren-web.renci.org/idrop-web/applet"
 idrop.config.idrop.lite.use.applet.dir=false
-
 
 /*
  * 4) iDROP web includes a link to launch the iDROP desktop GUI, using Java WebStart.  WebStart looks for a jnlp file and the accompanying 
@@ -58,7 +56,7 @@ idrop.config.idrop.lite.use.applet.dir=false
  * idrop.config.idrop.jnlp="https://lifetime-library.ils.unc.edu/llclient/idrop.jnlp"
  */
 
-idrop.config.idrop.jnlp="http://iren-web.renci.org:8080/idrop/idrop.jnlp"
+ idrop.config.idrop.jnlp="http://iren-web.renci.org:8080/idrop/idrop.jnlp"
 /*
  * Some properties may be set in an external configuration file, as configured below
  */
