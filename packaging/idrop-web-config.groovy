3430b1a672a9f599b1492a1a2ad10a656612bc5d
Lisa Stillwell <lisa@localhost.localdomain>
2012-08-09 16:10:41 -0400

    [#769] add packaging support for external groovy config file

--- packaging/idrop-web-config.groovy
+++ packaging/idrop-web-config.groovy
@@ -32,7 +32,7 @@ environments {
  */
 
 idrop.config.idrop.lite.applet.jar="idrop-lite-1.0.1-SNAPSHOT-jar-with-dependencies.jar"
-idrop.config.idrop.lite.codebase="http://127.0.0.1:8080/idrop-web/extras"
+idrop.config.idrop.lite.codebase="http://127.0.0.1/idrop-web-extras"
 idrop.config.idrop.lite.use.applet.dir=false
 
 /*
@@ -41,7 +41,7 @@ idrop.config.idrop.lite.use.applet.dir=false
  * 
  */
 
-idrop.config.idrop.jnlp="http://127.0.0.1:8080/idrop-web/extras/idrop.jnlp"
+idrop.config.idrop.jnlp="http://127.0.0.1/idrop-web-extras/idrop.jnlp"
 /*
  * 5) Customization properties
  */
