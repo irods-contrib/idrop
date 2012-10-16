d5bf19592d7a3e1f46c60ca0a569e4c5f5661923
Lisa Stillwell <lisa@lisa-VirtualBox.(none)>
2012-10-16 17:25:48 -0400

    [#769] experiment in setting up local repo

--- packaging/build.sh
+++ packaging/build.sh
@@ -45,6 +45,9 @@ UGLYSETTINGSFILESTRING='
   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
   xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
                       http://maven.apache.org/xsd/settings-1.0.0.xsd">
+  <localRepository>
+    /tmp/hudson-irodsbuild/workspace/.m2/repository
+  </localRepository>
   <proxies>
     <proxy>
       <active>true</active>
