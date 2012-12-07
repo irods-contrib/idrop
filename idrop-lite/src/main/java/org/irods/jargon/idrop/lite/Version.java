d588a5243775e957231ccd174fc6794c53da24c3
mconway <michael.c.conway@gmail.com>
2012-12-07 12:08:24 -0500

    [#1092] add reconnect option to iDrop

--- idrop-lite/src/main/java/org/irods/jargon/idrop/lite/Version.java
+++ idrop-lite/src/main/java/org/irods/jargon/idrop/lite/Version.java
@@ -1,5 +1,5 @@
 package org.irods.jargon.idrop.lite;
 public final class Version {
  public static String VERSION="1.0.3-SNAPSHOT";
- public static String BUILD_TIME="20121206-0814";
+ public static String BUILD_TIME="20121207-1122";
 }
