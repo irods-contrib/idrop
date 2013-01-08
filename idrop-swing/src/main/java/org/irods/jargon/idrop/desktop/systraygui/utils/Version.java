f70c85db3f779fff525ccb0be8b1712bcdb86959
Lisa Stillwell <lisa@renci.org>
2013-01-08 10:20:06 -0500

    [#983] fix versions - to 2.0.0

--- idrop-swing/src/main/java/org/irods/jargon/idrop/desktop/systraygui/utils/Version.java
+++ idrop-swing/src/main/java/org/irods/jargon/idrop/desktop/systraygui/utils/Version.java
@@ -1,5 +1,5 @@
 package org.irods.jargon.idrop.desktop.systraygui.utils;
 public final class Version {
- public static String VERSION="1.0.2-SNAPSHOT";
- public static String BUILD_TIME="20121213-1341";
+ public static String VERSION="2.0.0-SNAPSHOT";
+ public static String BUILD_TIME="20130108-1016";
 }
