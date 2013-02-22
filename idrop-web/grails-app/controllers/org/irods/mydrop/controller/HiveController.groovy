0f6fc79308ce81e7ee7ebe5ad01a7ce5ac7df642
koushyar <rajavi@email.unc.edu>
2013-02-22 11:49:05 -0500

    displaying the first set of concepts

--- idrop-web/grails-app/controllers/org/irods/mydrop/controller/HiveController.groovy
+++ idrop-web/grails-app/controllers/org/irods/mydrop/controller/HiveController.groovy
@@ -1,8 +1,12 @@
 package org.irods.mydrop.controller
 
+import java.util.List;
+
 import org.irods.jargon.core.connection.IRODSAccount
 import org.irods.jargon.core.pub.IRODSAccessObjectFactory
 import org.irods.jargon.hive.service.VocabularyService
+import org.unc.hive.client.ConceptProxy
+
 
 class HiveController {
 
@@ -38,8 +42,57 @@ class HiveController {
 		render(view:"vocabList", model:[vocabs:vocabs])
 	}
 	
-	def showTreeForSelectedVocabularies(){
-		render(view:"conceptBrowser")
+
+	 def showTreeForSelectedVocabularies(){
+	 log.info("getting first set of concepts")
+	 List<ConceptProxy> subTopConcept = vocabularyService.getSubTopConcept(params['selectedVocab'].toString().toLowerCase() , "A" , true)
+	 //params['selectedVocab']
+	 int sizeOfSubTopConcept = subTopConcept.size()
+	 List<String> listOfPreferedLabels = new ArrayList<String>()
+	 for (ConceptProxy concept : subTopConcept){
+		 listOfPreferedLabels.add(concept.preLabel)
+	 }
+	 render(view:"conceptBrowser", model:[listOfPreferedLabels:listOfPreferedLabels])
+ }
+
+
+	
+	def deleteSelectedVocabularies = {
+		log.info("deleteSelectedVocabularies")
+
+		log.info("params: ${params}")
+
+		def vocabulariesToDelete = params['selectedVocab']
+
+		// if nothing selected, just jump out and return a message
+		if (!ticketsToDelete) {
+			log.info("no vocabularies to delete")
+			render "OK"
+			return
+		}
+
+		//TicketAdminService ticketAdminService = ticketServiceFactory.instanceTicketAdminService(irodsAccount)
+
+		log.info("vocabularies: ${vocabulariesToDelete}")
+		if (vocabulariesToDelete instanceof Object[]) {
+			log.debug "is array"
+			vocabulariesToDelete.each{
+				log.info "vocabulariesToDelete: ${it}"
+				//ticketAdminService.deleteTicket(it)
+				//log.info("deleted:${it}")
+			}
+		} else {
+			log.debug "not array"
+			log.info "deleting: ${vocabulariesToDelete}..."
+			//ticketAdminService.deleteTicket(ticketsToDelete)
+			//log.info("deleted:${ticketsToDelete}")
+		}
+
+		render "OK"
 	}
+
+	
+	
+	
 	
 }
--- idrop-web/web-app/js/mydrop/hive.js
+++ idrop-web/web-app/js/mydrop/hive.js
@@ -54,3 +54,5 @@ function selectVocabularies(){
 	});
 	
 }
+
+
