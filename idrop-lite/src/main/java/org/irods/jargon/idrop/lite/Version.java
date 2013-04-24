604576812b5bb78d3cef6a487bbf6a225f343d97
mconway <michael.c.conway@gmail.com>
2013-04-24 10:19:29 -0400

    [#1250] jargon/idrop performance testing and optimization for 3.3.2

--- idrop-lite/src/main/java/org/irods/jargon/idrop/lite/Version.java
+++ idrop-lite/src/main/java/org/irods/jargon/idrop/lite/Version.java
@@ -1,5 +1,5 @@
 package org.irods.jargon.idrop.lite;
 public final class Version {
  public static String VERSION="2.0.1-SNAPSHOT";
- public static String BUILD_TIME="20130423-1515";
+ public static String BUILD_TIME="20130423-1550";
 }
