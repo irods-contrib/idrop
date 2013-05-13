05f6318d63bbd6dfa317266986b3120048cfdc7e
mconway <michael_conway@unc.edu>
2013-05-13 14:40:58 -0400

    [#964] merge development

--- idrop-lite/src/main/java/org/irods/jargon/idrop/lite/Version.java
+++ idrop-lite/src/main/java/org/irods/jargon/idrop/lite/Version.java
@@ -1,6 +1,5 @@
 package org.irods.jargon.idrop.lite;
 public final class Version {
  public static String VERSION="b964-SNAPSHOT";
- public static String BUILD_TIME="20130509-1342";
-
+ public static String BUILD_TIME="20130513-1439";
 }
