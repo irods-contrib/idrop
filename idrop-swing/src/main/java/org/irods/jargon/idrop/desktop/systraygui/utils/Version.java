10a889c04a3f96d880fce49cba6099d890c1e362
mconway <michael_conway@unc.edu>
2013-07-03 15:26:35 -0400

    [#964] merge latest

--- idrop-swing/src/main/java/org/irods/jargon/idrop/desktop/systraygui/utils/Version.java
+++ idrop-swing/src/main/java/org/irods/jargon/idrop/desktop/systraygui/utils/Version.java
@@ -1,11 +1,5 @@
 package org.irods.jargon.idrop.desktop.systraygui.utils;
 public final class Version {
-<<<<<<< HEAD
  public static String VERSION="b964-SNAPSHOT";
- public static String BUILD_TIME="20130605-1247";
-=======
-
- public static String VERSION="2.0.1-SNAPSHOT";
- public static String BUILD_TIME="20130605-1641";
->>>>>>> development
+ public static String BUILD_TIME="20130703-1526";
 }
