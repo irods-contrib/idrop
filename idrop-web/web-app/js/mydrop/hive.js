f72c2c54bc129a86636b4b7479fabea445c67c2a
mconway <michael.c.conway@gmail.com>
2013-07-12 14:08:41 -0400

    [#1460] restore css and image files

---
+++ idrop-web/web-app/js/mydrop/hive.js
@@ -0,0 +1,254 @@
+/**
+ * Show the hive view
+ * 
+ * @param absPath
+ * @returns {Boolean}
+ */
+function showHiveView(absPath, targetDiv) {
+	if (absPath == null) {
+		absPath = baseAbsPath;
+	}
+
+	try {
+		var params = {
+			absPath : absPath
+		}
+
+		lcSendValueWithParamsAndPlugHtmlInDiv("/hive/index", params,
+				"#infoAccordionHiveInner", null);
+
+	} catch (err) {
+		setErrorMessage(err);
+		setHiveNoData();
+	}
+
+}
+
+/**
+ * Apply the term, this actually throws up the dialog to entry a comment before the actual update occurs
+ * @param absPath
+ * @param vocabulary
+ * @param uri
+ * @returns {Boolean}
+ */
+function applyHiveTerm(absPath, vocabulary, uri) {
+	
+	if (absPath == null) {
+		setErrorMessage(jQuery.i18n.prop('msg_path_missing'));
+		return false;
+	}
+	
+	if (vocabulary == null) {
+		setErrorMessage(jQuery.i18n.prop('msg_no_vocabulary'));
+		return false;
+	}
+	
+	if (uri == null) {
+		setErrorMessage(jQuery.i18n.prop('msg_uri_missing'));
+		return false;
+	}
+	
+	var params = {
+			absPath : absPath,
+			vocabulary : vocabulary,
+			uri : uri
+		}
+
+		lcSendValueWithParamsAndPlugHtmlInDiv("/hive/hiveUpdateDialog", params, "", function(data) {
+			showHiveDetailsDialog(data);
+		});
+
+}
+
+function updateHiveTerm(absPath, vocabulary, uri, comment)  {
+	
+	if (uri == null || uri == "") {
+		setErrorMessage(jQuery.i18n.prop('msg_no_form_data'));
+		return false;
+	}
+	
+	if (vocabulary == null || vocabulary == "") {
+		setErrorMessage(jQuery.i18n.prop('msg_no_form_data'));
+		return false;
+	}
+
+	if (absPath == null || absPath == "") {
+		setErrorMessage(jQuery.i18n.prop('msg_path_missing'));
+		return false;
+	}
+	
+	if (comment == null) {
+		comment = "";
+	}
+	
+    showBlockingPanel();
+    
+    params = {
+    		uri:uri,
+    		absPath:absPath,
+    		vocabulary:vocabulary,
+    		comment:comment
+    }
+
+
+	var jqxhr = $.post(context + "/hive/applyHiveTerm", params,
+			function(data, status, xhr) {
+			}, "html").success(function(data, status, xhr) {
+				var continueReq = checkForSessionTimeout(data, xhr);
+				if (!continueReq) {
+					return false;
+				} 
+				$("#conceptBrowserDialog").html("").hide("slow");
+				$("#conceptBrowserPivotContainer").html(data);
+				$("#conceptBrowserMain").show("slow");
+				setMessage(jQuery.i18n.prop('msg_update_successful'));
+				unblockPanel();
+				
+	}).error(function(xhr, status, error) {
+		setErrorMessage(xhr.responseText);
+        unblockPanel();
+	});
+}
+
+/**
+ * Hide the concept browser view and show the hive details view
+ * @param data
+ */
+function showHiveDetailsDialog(data) {
+	$("#conceptBrowserMain").hide("slow");
+	$("#conceptBrowserDialog").html(data).show("slow");
+}
+
+/**
+ * Set the concept browser view to the given uri
+ * 
+ * @param uri
+ * @returns
+ */
+function browseToUri(uri, absPath) {
+
+	if (uri == null) {
+		setErrorMessage(jQuery.i18n.prop('msg_no_uri'));
+		return false;
+	}
+
+	if (absPath == null) {
+		setErrorMessage(jQuery.i18n.prop('msg_path_missing'));
+		return false;
+	}
+
+	try {
+		var params = {
+			targetURI : uri,
+			absPath : absPath
+		}
+
+		lcSendValueWithParamsAndPlugHtmlInDiv("/hive/conceptBrowserPivotView", params,
+				"#conceptBrowserPivotContainer", null);
+
+	} catch (err) {
+		setErrorMessage(err);
+		setHiveNoData();
+	}
+
+}
+
+/**
+ * Set the vocabulary to the top level of the provided vocabulary name. If no
+ * vocabulary is specified, the controller will pick the first of the selected
+ * vocabularies to display
+ * 
+ * @param absPath
+ *            current iRODS path
+ * @param vocabulary
+ *            HIVE vocabulary name
+ */
+function resetVocabulary(vocabulary, absPath) {
+
+	if (absPath == null) {
+		setErrorMessage(jQuery.i18n.prop('msg_path_missing'));
+		return false;
+	}
+
+	try {
+		var params;
+		if (vocabulary == null) {
+			params = {
+				absPath : absPath
+			}
+		} else {
+			params = {
+				absPath : absPath,
+				vocabulary : vocabulary
+			}
+		}
+		lcSendValueWithParamsAndPlugHtmlInDiv("/hive/resetConceptBrowser",
+				params, "#conceptBrowserPivotContainer", function(data) {
+					$("#conceptBrowserPivotContainer").html(data);
+				}, function() {
+					setHiveNoData();
+				});
+	} catch (err) {
+		setErrorMessage(err);
+		setHiveNoData();
+	}
+
+}
+
+function selectVocabularies() {
+	var formData = $("#hiveVocabularyForm").serializeArray();
+	if (formData == null) {
+		setErrorMessage(jQuery.i18n.prop('msg_no_form_data'));
+		return false;
+	}
+
+	lcShowBusyIconInDiv("#infoAccordionHiveInner");
+
+	var jqxhr = $.post(context + "/hive/selectVocabularies", formData,
+			function(data, status, xhr) {
+			}, "html").success(function(data, status, xhr) {
+		var continueReq = checkForSessionTimeout(data, xhr);
+		if (!continueReq) {
+			return false;
+		}
+
+		$("#infoAccordionHiveInner").html(data);
+
+	}).error(function(xhr, status, error) {
+		setErrorMessage(xhr.responseText);
+	});
+
+}
+
+function setHiveNoData() {
+	$("#infoAccordionHiveInner").html("No data to display");
+}
+
+function setLetter (indexLetter) {
+	var absPath = $("#infoAbsPath").val();
+
+	if (absPath == null) {
+		setErrorMessage(jQuery.i18n.prop('msg_path_missing'));
+		return false;
+	}
+	
+	var params = {
+		indexLetter:indexLetter,
+		absPath:absPath
+	}
+	
+	lcSendValueWithParamsAndPlugHtmlInDiv("/hive/conceptBrowserPivotView", params, "#conceptBrowserPivotContainer", null);
+	
+}
+
+function searchConcept(searchedConcept) {
+
+		var params = {
+			searchedConcept : searchedConcept
+		}
+
+	lcSendValueWithParamsAndPlugHtmlInDiv("/hive/searchConcept", params,
+			"#searchConceptResults", null);
+
+}
+
---
+++ idrop-web/web-app/js/mydrop/hivequery.js
@@ -0,0 +1,199 @@
+
+
+function hiveQueryByTerm(searchTerm) {
+	if (!searchTerm) {
+		setErrorMessage(jQuery.i18n.prop('msg_search_missing'));
+		return false;
+	}
+	
+	var params = {
+			uri:searchTerm
+		}
+	
+	var getUrl = "/sparqlQuery/searchByTerm";
+	 showBlockingPanel();
+	
+	$.get(context + getUrl, params, function(data, status, xhr) {
+		var continueReq = checkForSessionTimeout(data, xhr);
+		if (!continueReq) {
+			return false;
+		}
+		
+		processHiveQueryResults(data);
+		unblockPanel();
+		$('#searchTabs a[href="#resultsTab"]').tab('show');
+	}, "html").error(function(xhr, status, error) {
+		$(resultDiv).html("");
+		setErrorMessage(xhr.responseText);
+		unblockPanel();
+	});
+	
+	
+	
+}
+
+function hiveQueryByRelatedTerm(searchTerm) {
+	if (!searchTerm) {
+		setErrorMessage(jQuery.i18n.prop('msg_search_missing'));
+		return false;
+	}
+	
+	var params = {
+			uri:searchTerm
+		}
+	
+	var getUrl = "/sparqlQuery/searchByRelatedTerm";
+	 showBlockingPanel();
+	
+	$.get(context + getUrl, params, function(data, status, xhr) {
+		var continueReq = checkForSessionTimeout(data, xhr);
+		if (!continueReq) {
+			return false;
+		}
+		
+		processHiveQueryResults(data);
+		unblockPanel();
+		$('#searchTabs a[href="#resultsTab"]').tab('show');
+	}, "html").error(function(xhr, status, error) {
+		$(resultDiv).html("");
+		setErrorMessage(xhr.responseText);
+		unblockPanel();
+	});
+	
+	
+	
+}
+
+function processHiveQueryResults(data) {
+	
+	if (data == null || data == '') {
+		$("#resultsTabInner").html("");
+		setErrorMessage(jQuery.i18n.prop('msg_search_unsuccessful'))
+	}
+	
+	 showBlockingPanel();
+	
+	var myObject = $.parseJSON(data);
+	
+	// build first part of result table...
+	
+	var html = "<table id=\"searchResultTable\" class=\"table table-striped table-hover\" cellspacing=\"0\" cellpadding=\"0\" border=\"0\"><thead><tr><th></th><th>iRODS Path</th><th>VocabularyInfo</th></tr></thead>";
+	
+	html += "<tbody>";
+	
+	if (myObject.results != null && myObject.results.bindings != null) {
+	
+	$.each(myObject.results.bindings, function(index, value) {
+
+		html += "<tr><td>";
+		
+		if (value.infoLink != null && value.infoLink.value != null) {
+			//html += "<a href='" + value.infoLink.value + "'><i class=\"icon-folder-open\"></i></a>";
+			html += "<a href='" + context + "/browse/index?mode=path&absPath=" + encodeURIComponent(value.absPath.value) +"'><i class=\"icon-folder-open\"></i></a>";
+		}
+		
+		if (value.weblink != null && value.weblink.value != null) {
+			html += "<a href='" + value.weblink.value + "'><i class=\"icon-download\"></i></a>";
+		}
+		
+		
+		html += "</td><td>"; 
+			
+		html += value.absPath.value;
+		html += "</td><td>";
+		
+		if (value.y != null && value.y.value != null) {
+			html += value.y.value;
+		}
+		
+		html += "</td>";
+		html += "</tr>";
+	
+	}
+			
+	); 
+}
+	
+	html += "</tbody><tfoot><tr><td></td><td></td><td></td></tr></tfoot></table>";
+	
+	$("#resultsTabInner").html(html);
+	unblockPanel();
+
+
+}
+
+function hiveQuerySparqlReturnNewWindow(searchTerm) {
+	if (!searchTerm) {
+		setErrorMessage(jQuery.i18n.prop('msg_search_missing'));
+		return false;
+	}
+	
+	var params = {
+			query:searchTerm
+		}
+	
+	var getUrl = "/sparqlQuery/searchSparql";
+	$("#resultsTabInner").html("");
+	 showBlockingPanel();
+	
+	$.post(context + getUrl, params, function(data, status, xhr) {
+		var continueReq = checkForSessionTimeout(data, xhr);
+		if (!continueReq) {
+			return false;
+		}
+		
+		$("#resultsTabInner").html(data);
+		unblockPanel();
+		$('#searchTabs a[href="#resultsTab"]').tab('show');
+	}, "html").error(function(xhr, status, error) {
+		$(resultDiv).html("");
+		setErrorMessage(xhr.responseText);
+		unblockPanel();
+	});
+	
+}
+
+/*
+<div>
+
+	
+	<tbody>
+		<g:each in="${results}" var="entry">
+
+			<tr id="${entry.formattedAbsolutePath}">
+
+				<td><span
+					class="ui-icon-circle-plus search-detail-icon  ui-icon"></span></td>
+				<td><g:checkBox name="selectDetail"
+						value="${entry.formattedAbsolutePath}" checked="false" /> <span
+					class="setPaddingLeftAndRight"><g:link target="_blank"
+							controller="browse" action="index"
+							params="[mode: 'path', absPath: entry.formattedAbsolutePath]">
+							<i class="icon-folder-open "></i>
+						</g:link></span></td>
+				<td>
+					${entry.nodeLabelDisplayValue}
+				</td>
+				<td>
+					${entry.objectType}
+				</td>
+				<td>
+					${entry.displayDataSize}
+				</td>
+			</tr>
+		</g:each>
+
+	</tbody>
+
+	<tfoot>
+		<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+	</tfoot>
+</table>
+</div>
+*/
