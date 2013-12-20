49dfd159c2cadcb44948c1ed3d2139fc75b9e55b
mconway <michael_conway@unc.edu>
2013-12-20 12:10:44 -0500

    upgrade to grails 2.3.4

--- idrop-web3/grails-app/conf/BuildConfig.groovy
+++ idrop-web3/grails-app/conf/BuildConfig.groovy
@@ -55,19 +55,19 @@ grails.project.dependency.resolution = {
 	
 
     plugins {
-        runtime ":hibernate:$grailsVersion"
-        runtime ":jquery:1.8.3"
-        runtime ":resources:1.2"
+       // runtime ":hibernate:$grailsVersion"
+        //runtime ":jquery:1.8.3"
+        //runtime ":resources:1.2"
 
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0"
         //runtime ":cached-resources:1.0"
         //runtime ":yui-minify-resources:0.1.5"
 
-        build ":tomcat:$grailsVersion"
+       // build ":tomcat:$grailsVersion"
 
-        runtime ":database-migration:1.3.2"
+       // runtime ":database-migration:1.3.2"
 
-        compile ':cache:1.0.1'
+       // compile ':cache:1.0.1'
     }
 }
