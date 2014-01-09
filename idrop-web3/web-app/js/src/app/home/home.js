5e2c17ed5df2b8a918d6486a2fa5e583e701f609
mconway <michael_conway@unc.edu>
2014-01-09 16:33:50 -0500

    idrop3 fixed layout

--- idrop-web3/web-app/js/src/app/home/home.js
+++ idrop-web3/web-app/js/src/app/home/home.js
@@ -6,18 +6,37 @@
 
 angular.module('home', [])
 
-.config(function(){
+.config(function($scope){
 	/*
 	 * configuration block
 	 */
-})
-
-.controller('HomeCtrl', function($scope) {
-	/*
-	 * Home controller function here
-	 */
 	
+
 	$scope.name="mconway";
 	$scope.hideDrives="false";
+
+})
+
+/*
+	 * Home controller function here
+	 */
+.controller('HomeCtrl', function($scope) {
+	
+	
+	
+	/*
+	 * Cause the collections panel on the left to display
+	 */
+	$scope.showCollections = function () {
+		$scope.hideDrives="false";
+	};
+	
+	/*
+	 * Cause the collections panel on the left to be hidden
+	 */
+	$scope.hideCollections = function () {
+		$scope.hideDrives="true";
+	};
+	
 	
 });
