0e8a5709f7bb1e5a18ada5ba7d6c2987c3cd6db6
mconway <michael_conway@unc.edu>
2014-02-19 19:13:20 -0500

    first jasmine angular module test

---
+++ idrop3-js/js/test/unit/common/virtual-collections/virtualCollectionsServiceSpec.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+
+describe("A suite", function() {
+
+   var virtualCollectionsService;
+    beforeEach(module('virtualCollectionsModule'));
+    beforeEach(inject(function(_virtualCollectionsService_) {
+        virtualCollectionsService = _virtualCollectionsService_;
+    }))
+
+  it("list virtual collections should return a list of colls", function() {
+      var actual = virtualCollectionsService.listUserVirtualCollections();
+    expect(actual).toEqual('hello');
+  });
+});
