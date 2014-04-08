1289f18e2dd77e9bddf91164a792b66816bb5167
mconway <michael_conway@unc.edu>
2014-04-08 08:04:14 -0400

    init display of vcs with revised structure, icons

--- idrop-web3/web-app/js/app.js
+++ idrop-web3/web-app/js/app.js
@@ -72,25 +72,28 @@ var irodsAccount = function (host, port, zone, userName, password, authType, res
  * Created by mikeconway on 4/7/14.
  */
 
-angular.module('virtualCollectionFilter', []).filter('vcIconFilter', function () {
+angular.module('virtualCollectionFilter', []).filter('vcIcon', function ($log) {
     /**
-     * Given an input type which is a virtual collecion i18n icon name, convert to an appropriate icon css class
+     * Given an input type which is a virtual collection i18n icon name, convert to an appropriate icon css class
      */
 
     return function (inputType) {
        if (!inputType) {
+           $log.info("no type, use default icon");
            return "glyphicon-folder-close";
        } else if (inputType == "virtual.collection.default.icon") {
+           $log.info("use default");
            return "glyphicon-folder-close";
        } else if (inputType == "virtual.collection.icon.starred") {
+           $log.info("use star icon");
            return "glyphicon-star";
        } else {
+           $log.info("unknown type, use default");
            return "glyphicon-folder-close";
        }
 
     };
 });
-
 
 /**
  * Supporting code for integration with angular-translate
--- idrop3-js/js/app/common/filters/virtualCollectionsIconFilter.js
+++ idrop3-js/js/app/common/filters/virtualCollectionsIconFilter.js
@@ -3,19 +3,23 @@
  * Created by mikeconway on 4/7/14.
  */
 
-angular.module('virtualCollectionFilter', []).filter('vcIcon', function () {
+angular.module('virtualCollectionFilter', []).filter('vcIcon', function ($log) {
     /**
      * Given an input type which is a virtual collection i18n icon name, convert to an appropriate icon css class
      */
 
     return function (inputType) {
        if (!inputType) {
+           $log.info("no type, use default icon");
            return "glyphicon-folder-close";
        } else if (inputType == "virtual.collection.default.icon") {
+           $log.info("use default");
            return "glyphicon-folder-close";
        } else if (inputType == "virtual.collection.icon.starred") {
+           $log.info("use star icon");
            return "glyphicon-star";
        } else {
+           $log.info("unknown type, use default");
            return "glyphicon-folder-close";
        }
 
