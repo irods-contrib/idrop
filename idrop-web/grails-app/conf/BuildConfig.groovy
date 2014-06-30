c7e8261191756745b8762a2f29ccb8047299fdab
mconway <michael.c.conway@gmail.com>
2014-06-30 14:23:00 -0400

    #23 update to jargon master

--- idrop-web/grails-app/conf/BuildConfig.groovy
+++ idrop-web/grails-app/conf/BuildConfig.groovy
@@ -31,25 +31,25 @@ grails.project.dependency.resolution = {
 		compile 'commons-io:commons-io:2.1'
 		provided 'junit:junit:4.8.1'
 		test 'org.mockito:mockito-all:1.8.1'
-		compile ('org.irods.jargon:jargon-core:3.3.5-SNAPSHOT') { excludes ([group:'org.jglobus'])}
-		compile ('org.irods.jargon:jargon-data-utils:3.3.5-SNAPSHOT')  { excludes ([group:'org.jglobus'])}
-		compile ('org.irods.jargon:jargon-ticket:3.3.5-SNAPSHOT')   { excludes ([group:'org.jglobus'])}
-		compile ('org.irods.jargon:jargon-user-profile:3.3.5-SNAPSHOT')  { excludes ([group:'org.jglobus'])}
-		compile ('org.irods.jargon:jargon-user-tagging:3.3.5-SNAPSHOT')  { excludes ([group:'org.jglobus'])}
-		compile ('org.irods.jargon:jargon-workflow:3.3.5-SNAPSHOT')  { excludes ([group:'org.jglobus'])}
-		compile ('org.irods.jargon:jargon-ruleservice:3.3.5-SNAPSHOT')  { excludes ([group:'org.jglobus'])}
+		compile ('org.irods.jargon:jargon-core:4.0.2-SNAPSHOT') { excludes ([group:'org.jglobus'])}
+		compile ('org.irods.jargon:jargon-data-utils:4.0.2-SNAPSHOT')  { excludes ([group:'org.jglobus'])}
+		compile ('org.irods.jargon:jargon-ticket:4.0.2-SNAPSHOT')   { excludes ([group:'org.jglobus'])}
+		compile ('org.irods.jargon:jargon-user-profile:4.0.2-SNAPSHOT')  { excludes ([group:'org.jglobus'])}
+		compile ('org.irods.jargon:jargon-user-tagging:4.0.2-SNAPSHOT')  { excludes ([group:'org.jglobus'])}
+		compile ('org.irods.jargon:jargon-workflow:4.0.2-SNAPSHOT')  { excludes ([group:'org.jglobus'])}
+		compile ('org.irods.jargon:jargon-ruleservice:4.0.2-SNAPSHOT')  { excludes ([group:'org.jglobus'])}
 
 		provided 'javax.servlet:servlet-api:2.5'
 		compile 'org.jsoup:jsoup:0.2.2'
 
 		compile 'org.springframework:spring-expression:4.0.5.RELEASE'
 		compile 'org.springframework:spring-aop:4.0.5.RELEASE'
-		
+
 
 		compile( group: 'log4j', name: 'log4j', version: '1.2.16', export: false )
 
 	}
-	
+
 	plugins {
 		build ":tomcat:7.0.52.1"
 		runtime ":hibernate:3.6.10.10"
