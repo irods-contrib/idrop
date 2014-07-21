113d64b75723301b44e7d91830ec76d9dd77ee0f
mconway <michael_conway@unc.edu>
2014-07-21 12:30:33 -0400

    #37 cleanups and fix gui at startup issue

--- idrop-web/grails-app/conf/spring/resources.groovy
+++ idrop-web/grails-app/conf/spring/resources.groovy
@@ -4,12 +4,12 @@ beans = {
 	profileService(org.irods.mydrop.service.ProfileService)  { irodsAccessObjectFactory = ref("irodsAccessObjectFactory") }
 
 	starringService(org.irods.mydrop.service.StarringService)  { irodsAccessObjectFactory = ref("irodsAccessObjectFactory") }
-	
+
 	sharingService(org.irods.mydrop.service.SharingService)  { irodsAccessObjectFactory = ref("irodsAccessObjectFactory") }
-	
+
 	ruleProcessingService(org.irods.mydrop.service.RuleProcessingService)  { irodsAccessObjectFactory = ref("irodsAccessObjectFactory") }
-	
-	 
+
+
 	browseController(org.irods.mydrop.controller.BrowseController) {
 		irodsAccessObjectFactory = ref("irodsAccessObjectFactory")
 		taggingServiceFactory = ref("taggingServiceFactory")
@@ -46,19 +46,13 @@ beans = {
 	auditController(org.irods.mydrop.controller.AuditController) { irodsAccessObjectFactory = ref("irodsAccessObjectFactory") }
 
 	profileController(org.irods.mydrop.controller.ProfileController) { irodsAccessObjectFactory = ref("irodsAccessObjectFactory") }
-	
-	homeController(org.irods.mydrop.controller.HomeController) {
-		irodsAccessObjectFactory = ref("irodsAccessObjectFactory")
-	}
-	
-	ruleController(org.irods.mydrop.controller.RuleController) {
-		irodsAccessObjectFactory = ref("irodsAccessObjectFactory")
-	}
 
-	
-	shoppingCartController(org.irods.mydrop.controller.ShoppingCartController) {
-		irodsAccessObjectFactory = ref("irodsAccessObjectFactory")
-	}
+	homeController(org.irods.mydrop.controller.HomeController) { irodsAccessObjectFactory = ref("irodsAccessObjectFactory") }
+
+	ruleController(org.irods.mydrop.controller.RuleController) { irodsAccessObjectFactory = ref("irodsAccessObjectFactory") }
+
+
+	shoppingCartController(org.irods.mydrop.controller.ShoppingCartController) { irodsAccessObjectFactory = ref("irodsAccessObjectFactory") }
 
 
 }
