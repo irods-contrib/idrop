8f8c2b30734411030bb3955f85964ec65cf009a5
mconway <michael.c.conway@gmail.com>
2014-10-27 11:27:52 -0400

    #14 prepare new iplant builds

--- idrop-swing/src/main/java/org/irods/jargon/idrop/desktop/systraygui/utils/Version.java
+++ idrop-swing/src/main/java/org/irods/jargon/idrop/desktop/systraygui/utils/Version.java
@@ -1,5 +1,5 @@
 package org.irods.jargon.idrop.desktop.systraygui.utils;
 public final class Version {
  public static String VERSION="2.0.1-RC1";
- public static String BUILD_TIME="20141010-0751";
+ public static String BUILD_TIME="20141027-1101";
 }
