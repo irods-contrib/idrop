ceb67adca20ad1b41649a08a1da8987cf6f865ce
mconway <michael_conway@unc.edu>
2014-11-03 12:00:57 -0500

    #41 fix file size, also drop version files from commit

--- idrop-swing/src/main/java/org/irods/jargon/idrop/desktop/systraygui/utils/Version.java
+++
@@ -1,5 +0,0 @@
-package org.irods.jargon.idrop.desktop.systraygui.utils;
-public final class Version {
- public static String VERSION="2.0.1-RC1";
- public static String BUILD_TIME="20141027-1101";
-}
