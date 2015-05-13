93cffbb843429294d8ed1b811ddf07731458b1e4
michael-conway <Michael.C.Conway@gmail.com>
2015-05-13 14:49:12 -0400

    #3 fix dependencies

--- idrop-swing/src/main/java/org/irods/jargon/idrop/desktop/systraygui/utils/Version.java
+++ idrop-swing/src/main/java/org/irods/jargon/idrop/desktop/systraygui/utils/Version.java
@@ -1,5 +1,5 @@
 package org.irods.jargon.idrop.desktop.systraygui.utils;
 public final class Version {
  public static String VERSION="2.0.1-SNAPSHOT";
- public static String BUILD_TIME="20140626-1010";
+ public static String BUILD_TIME="2015-05-13T18:12:18Z";
 }
