081d2d5810fd939a4d21371d835cd4c82ee04ddb
Mike Conway <Michael.c.conway@gmail.com>
2017-11-15 09:43:19 -0500

    Update idrop.bat

--- idrop-swing/installer/idrop.bat
+++ idrop-swing/installer/idrop.bat
@@ -1,2 +1,2 @@
 
-java -classpath "idrop-swing-4.2.0.0-SNAPSHOT.jar;lib/*"  org.irods.jargon.idrop.desktop.systraygui.IDROPDesktop
+java -classpath "idrop-swing-4.2.0.1-SNAPSHOT.jar;lib/*"  org.irods.jargon.idrop.desktop.systraygui.IDROPDesktop
